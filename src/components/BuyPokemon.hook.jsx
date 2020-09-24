a4b9c87eb22874aa9fae40759c4e7d6a052d19cc
julio jose maldonado <vessal27@hotmail.com>
2020-09-24 20:57:22 -0300

    arreglo warning classname

--- src/components/BuyPokemon.hook.jsx
+++ src/components/BuyPokemon.hook.jsx
@@ -7,8 +7,8 @@ const BuyPokemonHook = () => {
     const game_shop = useSelector((state) => state.game_shop);
     const dispatch = useDispatch();
     return(
-        <div className="row">
-            <button className="btn btn-dark btn-sm" disabled={game_shop.stockPokemon<1} onClick={()=>{
+        <div>
+            <button className="btn btn-dark btn-sm mb-2" disabled={game_shop.stockPokemon<1} onClick={()=>{
                 dispatch(buy_pokemon(1));
             }}> Buy Pokemon</button>
             <button className="btn btn-dark btn-sm" disabled={game_shop.stockPokemon>=game_shop.initStockPokemon} onClick={()=>{
